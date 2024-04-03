7101848ff0a43368527dfd659bff00f70db5e5c9
ca20110820 <ca20110820@gmail.com>
2024-04-03 14:39:13 +0800

    feat: added module for queue

---
+++ src/my_package/data_structures/queue.py
@@ -0,0 +1,19 @@
+class Queue:
+    def __init__(self, initializer=None):
+        self._list = initializer or []
+
+    @property
+    def peek(self):
+        return self._list[-1] if len(self._list) > 0 else None
+
+    def __str__(self):
+        return f"{self._list}"
+
+    def get_item_at_index(self, idx):
+        return self._list[idx]
+
+    def enqueue(self, elem):
+        self._list.append(elem)
+
+    def dequeue(self):
+        self._list = self._list[1:]
