1accd8c93b895b3a45ae3859cf2d48e2f9d5900f
ca20110820 <ca20110820@gmail.com>
2024-04-03 14:37:51 +0800

    feat: add module for stack

---
+++ src/my_package/data_structures/stack.py
@@ -0,0 +1,16 @@
+class Stack:
+    def __init__(self, initializer=None):
+        self._list = initializer or []
+
+    def __str__(self):
+        return f"{self._list}"
+
+    @property
+    def top(self):
+        return self._list[-1]
+
+    def pop(self):
+        return self._list.pop(-1) if len(self._list) > 0 else None
+
+    def append(self, elem):
+        self._list.append(elem)
