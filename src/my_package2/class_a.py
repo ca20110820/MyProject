4e80aaef85a814497440adc35ef93632c019df8a
ca20110820 <ca20110820@gmail.com>
2024-04-07 12:43:36 +0800

    refactor(ClassA): changed addition as a static method

--- src/my_package2/class_a.py
+++ src/my_package2/class_a.py
@@ -1,9 +1,12 @@
 """Module for ClassA"""
 
+
 class ClassA:
     """This is the ClassA"""
+
     def __init__(self):
         pass
-    
-    def addition(self, a, b):
+
+    @staticmethod
+    def addition(a, b):
         return a + b
